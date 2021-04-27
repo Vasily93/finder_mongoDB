7c0e2ea7e674368c334fd390db258caec87561aa
Vasily93 <markovavasily@gmail.com>
2021-04-27 10:48:15 -0700

    init testing with mocha

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send(`Listnening on port ${port}`)
+app.get('/api', (req, res) => {
+    res.send({hi: 'There'})
 })
 
 app.listen(port, () => {
     console.log(`Listnening on port ${port}`)
 });
 
+module.exports = app;
